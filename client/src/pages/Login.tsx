b889ae9736693ad85dc44b8cd8e714e72ae76285
Jace Galloway <jace.galloway@gmail.com>
2024-11-29 14:58:49 -0500

    handled login page validations

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,6 +2,7 @@ import { useState, FormEvent, ChangeEvent } from "react";
 
 import Auth from '../utils/auth';
 import { login } from "../api/authAPI";
+import { log } from "tone/build/esm/core/util/Debug";
 
 const Login = () => {
   const [loginData, setLoginData] = useState({
@@ -9,17 +10,50 @@ const Login = () => {
     password: ''
   });
 
+  const [errors, setErrors] = useState({
+    username: '',
+    password: ''
+  });
+
+  const showErrors = (name: string, value: string) => {
+    let isValid = true;
+
+    if (value.trim() === '') {
+      isValid = false;
+      setErrors((prev) => ({
+        ...prev,
+        [name]: `${name} is required`
+      }));
+    }
+    else {
+      setErrors((prev) => ({
+        ...prev,
+        [name]: ''
+      }));
+    }
+    return isValid;
+  }
+
   const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = e.target;
     setLoginData({
       ...loginData,
       [name]: value
     });
+    showErrors(name, value);
   };
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     try {
+      const isValidResults = Object.keys(loginData).filter(key => {
+        return !showErrors(key, loginData[key as keyof typeof loginData]);
+      })
+
+      if (isValidResults.length > 0) {
+        return;
+      }
+
       const data = await login(loginData);
       Auth.login(data.token);
     } catch (err) {
@@ -31,6 +65,8 @@ const Login = () => {
     <div className='container'>
       <form className='form' onSubmit={handleSubmit}>
         <h1>Login</h1>
+        {/* {JSON.stringify(loginData)}
+        {JSON.stringify(errors)}*/}
         <label >Username</label>
         <input 
           type='text'
@@ -38,6 +74,7 @@ const Login = () => {
           value={loginData.username || ''}
           onChange={handleChange}
         />
+        {errors.username && <p className='error'>{errors.username}</p>}
       <label>Password</label>
         <input 
           type='password'
@@ -45,6 +82,7 @@ const Login = () => {
           value={loginData.password || ''}
           onChange={handleChange}
         />
+        {errors.password && <p className='error'>{errors.password}</p>}
         <button type='submit'>Submit Form</button>
       </form>
     </div>
