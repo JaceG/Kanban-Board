69913341d18399862f43d194f1746a23ecf8502d
Jace Galloway <jace.galloway@gmail.com>
2024-12-07 14:19:39 -0500

    updated css

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,45 +1,54 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import auth from '../utils/auth';
 
 const Navbar = () => {
-  const [ loginCheck, setLoginCheck ] = useState(false);
+	const [loginCheck, setLoginCheck] = useState(false);
+	const location = useLocation();
 
-  const checkLogin = () => {
-    if(auth.loggedIn()) {
-      setLoginCheck(true);
-    }
-  };
+	const checkLogin = () => {
+		if (auth.loggedIn()) {
+			setLoginCheck(true);
+		}
+	};
 
-  useEffect(() => {
-    console.log(loginCheck);
-    checkLogin();
-  }, [loginCheck])
+	useEffect(() => {
+		console.log(loginCheck);
+		checkLogin();
+	}, [loginCheck]);
 
-  return (
-    <div className='nav'>
-      <div className='nav-title'>
-        <Link to='/'>Krazy Kanban Board</Link>
-      </div>
-      <ul>
-      {
-        !loginCheck ? (
-          <li className='nav-item'>
-            <button type='button'>
-              <Link to='/login'>Login</Link>
-            </button>
-          </li>
-        ) : (
-          <li className='nav-item'>
-            <button type='button' onClick={() => {
-              auth.logout();
-            }}>Logout</button>
-          </li>
-        )
-      }
-      </ul>
-    </div>
-  )
-}
+	return (
+		<div className='nav'>
+			<div className='nav-title'>
+				<Link to='/'>Krazy Kanban Board</Link>
+			</div>
+			<ul>
+				{loginCheck && !['/create'].includes(location.pathname) ? (
+					<button type='button' id='create-ticket-link'>
+						<Link to='/create'>New Ticket</Link>
+					</button>
+				) : null}
+				{!loginCheck ? (
+					<li className='nav-item'>
+						<button type='button'>
+							<Link to='/login'>Login</Link>
+						</button>
+					</li>
+				) : (
+					<li className='nav-item'>
+						<button
+							type='button'
+							className='logout-btn'
+							onClick={() => {
+								auth.logout();
+							}}>
+							Logout
+						</button>
+					</li>
+				)}
+			</ul>
+		</div>
+	);
+};
 
 export default Navbar;
